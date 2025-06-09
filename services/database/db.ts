a8cc51cdb5fa3f915bdeb1ed3594571e5ce4c04f
Matias Cardozo <matiascardozo14300@gmail.com>
2025-06-09 12:25:09 -0300

    Tablas borradas y creadas

--- services/database/db.ts
+++ services/database/db.ts
@@ -141,12 +141,60 @@ export async function importDatabaseFromJson() {
 
 	try {
 		await db.execAsync(`
-			DELETE FROM workout_type_exercises;
-			DELETE FROM sets;
-			DELETE FROM exercise_records;
-			DELETE FROM exercises;
-			DELETE FROM workouts;
-			DELETE FROM workout_types;
+			DROP TABLE IF EXISTS workout_type_exercises;
+			DROP TABLE IF EXISTS sets;
+			DROP TABLE IF EXISTS exercise_records;
+			DROP TABLE IF EXISTS exercises;
+			DROP TABLE IF EXISTS workouts;
+			DROP TABLE IF EXISTS workout_types;
+		`);
+
+		await db.execAsync(`
+			CREATE TABLE IF NOT EXISTS workout_types (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL UNIQUE,
+			isCustom INTEGER NOT NULL DEFAULT 0
+			);
+
+			CREATE TABLE IF NOT EXISTS exercises (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				name TEXT NOT NULL,
+				code TEXT NOT NULL UNIQUE,
+				muscleGroup TEXT NOT NULL,
+				favorite INTEGER NOT NULL DEFAULT 0
+			);
+
+			CREATE TABLE IF NOT EXISTS workout_type_exercises (
+				workoutTypeId INTEGER NOT NULL,
+				exerciseId INTEGER NOT NULL,
+				PRIMARY KEY (workoutTypeId, exerciseId),
+				FOREIGN KEY (workoutTypeId) REFERENCES workout_types(id),
+				FOREIGN KEY (exerciseId) REFERENCES exercises(id)
+			);
+
+			CREATE TABLE IF NOT EXISTS workouts (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				startDate TEXT NOT NULL,
+				finishDate TEXT NOT NULL,
+				workoutTypeId INTEGER NOT NULL,
+				FOREIGN KEY (workoutTypeId) REFERENCES workout_types(id)
+			);
+
+			CREATE TABLE IF NOT EXISTS exercise_records (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				workoutId INTEGER NOT NULL,
+				exerciseId INTEGER NOT NULL,
+				FOREIGN KEY (workoutId) REFERENCES workouts(id),
+				FOREIGN KEY (exerciseId) REFERENCES exercises(id)
+			);
+
+			CREATE TABLE IF NOT EXISTS sets (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				exerciseRecordId INTEGER NOT NULL,
+				weight REAL NOT NULL,
+				reps INTEGER NOT NULL,
+				FOREIGN KEY (exerciseRecordId) REFERENCES exercise_records(id)
+			);
 		`);
 
 		// 5) Insertar filas validadas
